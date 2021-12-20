855fef95206e80030d2b5bc04d6beb0dc5000ac9
Bartosz Karaś <bkaras2112@gmail.com>
2021-12-20 09:29:07 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        wlasciciel me = new wlasciciel();
+
+        me.budget=5000;
+        System.out.println(me.budget);
+
+
+    }
+}
---
+++ src/com/company/wlasciciel.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class wlasciciel {
+    int budget;
+    boolean java = true;
+    boolean data_base = true;
+    boolean frontend = true;
+    boolean wordpress = true;
+    boolean prestashop = true;
+    boolean mobile_app = false;
+}
